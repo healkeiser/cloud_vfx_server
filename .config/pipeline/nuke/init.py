bec5ba782094c50be03e7bd1b24cd1f53eb88a84
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2022-12-20 17:55:18 +0100

    Update init.py

--- .config/pipeline/nuke/init.py
+++ .config/pipeline/nuke/init.py
@@ -12,7 +12,9 @@ __email__ = "valentin.onze@gmail.com"
 
 def loadPlugins():
   nuke_path = os.getenv("NUKE_PATH")
-  nuke.pluginAddPath(os.path.join(nuke_path, "plugins/plugin_name"))
+  plugin_path = os.path.join(nuke_path, "plugins/plugin_name")
+  if os.path.isdir(plugin_path):
+    nuke.pluginAddPath(plugin_path)
 
 if __name__ == "__main__":
     loadPlugins()
