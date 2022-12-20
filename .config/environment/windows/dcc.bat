2b64058eeb14fa742e9948c2b31c030f9f8d0a64
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2022-12-20 12:36:58 +0100

    Create dcc.bat

---
+++ .config/environment/windows/dcc.bat
@@ -0,0 +1,52 @@
+:: ------ Description: Variables , environment and functions for the DCCs.
+:: ------ Author: Valentin Beaumont
+:: ------ Mail:valentin.onze@gmail.com
+
+@echo off
+
+:: ------ Cloud (used as a server equivalent)
+echo "Cloud - SERVER_ROOT:"
+setx SERVER_ROOT "Z:\My Drive"
+echo %SERVER_ROOT%
+echo.
+
+echo "Cloud - CONFIG_ROOT:"
+setx CONFIG_ROOT "%SERVER_ROOT%\.config"
+echo %CONFIG_ROOT%
+echo.
+
+echo "Cloud - ENVIRONMENT_ROOT:"
+setx ENVIRONMENT_ROOT "%CONFIG_ROOT%\environment"
+echo %ENVIRONMENT_ROOT%
+echo.
+
+echo "Cloud - PIPELINE_ROOT:"
+setx PIPELINE_ROOT "%CONFIG_ROOT%\pipeline"
+echo  %PIPELINE_ROOT%
+echo.
+
+:: ------ Houdini
+echo "Houdini - HSITE:"
+setx HSITE "%PIPELINE_ROOT%\houdini"
+echo %HSITE%
+echo.
+
+:: ------ Maya
+echo "Maya - MAYA_APP_DIR:"
+setx MAYA_APP_DIR "%PIPELINE_ROOT%\maya"
+echo %MAYA_APP_DIR%
+echo.
+
+:: ------ Nuke
+echo "Nuke - NUKE_PATH:"
+setx NUKE_PATH "%PIPELINE_ROOT%\nuke"
+echo %NUKE_PATH%
+echo.
+
+:: ------ Substance Painter
+echo "Substance Painter - SUBSTANCE_PAINTER_PLUGINS_PATH:"
+setx SUBSTANCE_PAINTER_PLUGINS_PATH "%PIPELINE_ROOT%\substance_painter\python"
+echo %SUBSTANCE_PAINTER_PLUGINS_PATH%
+echo.
+
+pause
