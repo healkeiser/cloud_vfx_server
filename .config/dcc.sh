d26c7173a93534a1df040f5dcc25853d4571c318
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2022-12-20 12:29:40 +0100

    Create dcc.sh

---
+++ .config/dcc.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+###### Description: Variables , environment and functions for the DCCs.
+###### Author: Valentin Beaumont
+###### Mail: valentin.onze@gmail.com
+
+# ------ Houdini
+export HSITE="$PIPELINE_ROOT/houdini"
+
+# ------ Maya
+export MAYA_APP_DIR="$PIPELINE_ROOT/maya"
+
+# ------ Nuke
+export NUKE_PATH="$PIPELINE_ROOT/nuke"
+
+# ------ Substance Painter
+export SUBSTANCE_PAINTER_PLUGINS_PATH="$PIPELINE_ROOT/substance_painter/python"
