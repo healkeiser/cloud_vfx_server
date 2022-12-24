7f7f7b2e4cfa24526d8fa974200ee19a7929a51c
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2022-12-24 01:27:46 +0100

    Update aliases.sh

--- .config/environment/unix/aliases.sh
+++ .config/environment/unix/aliases.sh
@@ -9,21 +9,21 @@ alias c="clear"
 alias h="history"
 alias j="jobs -l"
 
+# ------ DCCs and VFX
+alias {houdini,hou}='cd $ENVIRONMENT_ROOT/unix && sh ./houdini.sh'
+# alias {nuke,nk}='cd $ENVIRONMENT_ROOT/unix && sh ./nuke.sh'
+# alias {maya,ma}='cd $ENVIRONMENT_ROOT/unix && sh ./maya.sh'
+# alias {substance_painter,sp}='cd $ENVIRONMENT_ROOT/unix && sh ./substance_painter.sh'
+
 # ------ Add alias as per OS using $_myos
 _myos="$(uname)"
 case $_myos in
 Linux) ###### LINUX ############################################################
     alias current_os="echo 'linux'"
-
-    # ------ DCCs and VFX
-    alias {houdini, hou, h}='cd $ENVIRONMENT_ROOT/unix && sh ./houdini.sh'
     ;;
 
 Darwin) ###### MACOS ###########################################################
     alias current_os="echo 'macOS'"
-
-    # ------ DCCs and VFX
-    alias houdini="cd $ENVIRONMENT_ROOT/unix && sh ./houdini.sh"
     ;;
 
 Windows) ###### WINDOWS ########################################################
