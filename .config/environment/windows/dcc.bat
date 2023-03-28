99d5c4f479be28e829db41c972cd07db19d09bc1
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2023-03-28 16:23:39 +0100

    Update dcc.bat

--- .config/environment/windows/dcc.bat
+++ .config/environment/windows/dcc.bat
@@ -1,58 +1,57 @@
-:: ------ Description: Variables , environment and functions for the DCCs.
-:: ------ Author: Valentin Beaumont
-:: ------ Mail:valentin.onze@gmail.com
+:: Description: Variables, environment and functions for the DCCs.
+:: Author: Valentin Beaumont
+:: Mail:valentin.onze@gmail.com
 
 @echo off
 
-:: ------ Cloud (used as a server equivalent)
+:: Cloud (used as a server equivalent)
+echo.
 echo "Cloud - SERVER_ROOT:"
-setx SERVER_ROOT "Path\to\drive\windows"
-echo %SERVER_ROOT%
+set SERVER_ROOT "Path\to\Cloud"
+setx SERVER_ROOT "%SERVER_ROOT%"
+:: echo %SERVER_ROOT%
 echo.
 
 echo "Cloud - CONFIG_ROOT:"
-setx CONFIG_ROOT "%SERVER_ROOT%\.config"
-echo %CONFIG_ROOT%
+set CONFIG_ROOT "%SERVER_ROOT%\.config"
+setx CONFIG_ROOT "%CONFIG_ROOT%"
+:: echo %CONFIG_ROOT%
 echo.
 
 echo "Cloud - ENVIRONMENT_ROOT:"
 setx ENVIRONMENT_ROOT "%CONFIG_ROOT%\environment"
-echo %ENVIRONMENT_ROOT%
+:: echo %ENVIRONMENT_ROOT%
 echo.
 
 echo "Cloud - PIPELINE_ROOT:"
-setx PIPELINE_ROOT "%CONFIG_ROOT%\pipeline"
-echo  %PIPELINE_ROOT%
+set PIPELINE_ROOT "%CONFIG_ROOT%\pipeline"
+setx PIPELINE_ROOT "%PIPELINE_ROOT%"
+:: echo %PIPELINE_ROOT%
 echo.
 
-:: ------ Create $HOME for easier integration with Unix systems
-echo "Home - HOME:"
-setx HOME "C:\Users\%USERNAME%"
-echo  %HOME%
-echo.
-
-:: ------ Houdini
+:: Houdinih
 echo "Houdini - HSITE:"
 setx HSITE "%PIPELINE_ROOT%\houdini"
-echo %HSITE%
+:: echo %HSITE%
 echo.
 
-:: ------ Maya
+:: Maya
 echo "Maya - MAYA_APP_DIR:"
 setx MAYA_APP_DIR "%PIPELINE_ROOT%\maya"
-echo %MAYA_APP_DIR%
+:: echo %MAYA_APP_DIR%
 echo.
 
-:: ------ Nuke
+:: Nuke
 echo "Nuke - NUKE_PATH:"
 setx NUKE_PATH "%PIPELINE_ROOT%\nuke"
-echo %NUKE_PATH%
+:: echo %NUKE_PATH%
 echo.
 
-:: ------ Substance Painter
+:: Substance Painter
 echo "Substance Painter - SUBSTANCE_PAINTER_PLUGINS_PATH:"
 setx SUBSTANCE_PAINTER_PLUGINS_PATH "%PIPELINE_ROOT%\substance_painter\python"
-echo %SUBSTANCE_PAINTER_PLUGINS_PATH%
+:: echo %SUBSTANCE_PAINTER_PLUGINS_PATH%
 echo.
 
 pause
+
