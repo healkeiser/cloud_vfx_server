07d3171c9afbf6579e6a6e24a6662c412442b865
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2023-09-09 17:17:16 +0200

    Update dcc.bat

--- .config/environment/windows/dcc.bat
+++ .config/environment/windows/dcc.bat
@@ -1,57 +1,80 @@
-:: Description: Variables, environment and functions for the DCCs.
+:: Description: Set environment variables for various applications.
 :: Author: Valentin Beaumont
-:: Mail:valentin.onze@gmail.com
+:: Mail: valentin.onze@gmail.com
 
 @echo off
+setlocal
 
-:: Cloud (used as a server equivalent)
-echo.
-echo "Cloud - SERVER_ROOT:"
-set SERVER_ROOT "Path\to\Cloud"
-setx SERVER_ROOT "%SERVER_ROOT%"
-:: echo %SERVER_ROOT%
-echo.
+:: Temporary values, only valid in this shell
+:: Set variables (use quotes around values with spaces)
+set "SERVER_ROOT=C:\Users\valen\OneDrive"
+set "CONFIG_ROOT=%%SERVER_ROOT%%\.config"
+set "ENVIRONMENT_ROOT=%%CONFIG_ROOT%%\environment"
+set "PIPELINE_ROOT=%%CONFIG_ROOT%%\pipeline"
 
-echo "Cloud - CONFIG_ROOT:"
-set CONFIG_ROOT "%SERVER_ROOT%\.config"
-setx CONFIG_ROOT "%CONFIG_ROOT%"
-:: echo %CONFIG_ROOT%
-echo.
+set "HSITE=%%PIPELINE_ROOT%%\houdini"
+set "MAYA_APP_DIR=%%PIPELINE_ROOT%%\maya"
+set "NUKE_PATH=%%PIPELINE_ROOT%%\nuke"
+set "SUBSTANCE_PAINTER_PLUGINS_PATH=%%PIPELINE_ROOT%%\substance_painter\python"
 
-echo "Cloud - ENVIRONMENT_ROOT:"
-setx ENVIRONMENT_ROOT "%CONFIG_ROOT%\environment"
-:: echo %ENVIRONMENT_ROOT%
+:: Header
 echo.
-
-echo "Cloud - PIPELINE_ROOT:"
-set PIPELINE_ROOT "%CONFIG_ROOT%\pipeline"
-setx PIPELINE_ROOT "%PIPELINE_ROOT%"
-:: echo %PIPELINE_ROOT%
+powershell write-host -foregroundcolor White "Cloud VFX Server"
+echo https://github.com/healkeiser/cloud_vfx_server
 echo.
 
-:: Houdinih
-echo "Houdini - HSITE:"
-setx HSITE "%PIPELINE_ROOT%\houdini"
-:: echo %HSITE%
-echo.
+:: Lasting values, saved in the system
+:: Set environment variables
+setx "SERVER_ROOT" "%SERVER_ROOT%"
+powershell write-host -foregroundcolor Green "SERVER_ROOT: %SERVER_ROOT%"
 
-:: Maya
-echo "Maya - MAYA_APP_DIR:"
-setx MAYA_APP_DIR "%PIPELINE_ROOT%\maya"
-:: echo %MAYA_APP_DIR%
-echo.
+setx "CONFIG_ROOT" "%CONFIG_ROOT%"
+powershell write-host -foregroundcolor Green "CONFIG_ROOT: %CONFIG_ROOT%"
+powershell write-host -foregroundcolor Green "-> Expanded: %SERVER_ROOT%%\.config"
 
-:: Nuke
-echo "Nuke - NUKE_PATH:"
-setx NUKE_PATH "%PIPELINE_ROOT%\nuke"
-:: echo %NUKE_PATH%
-echo.
+setx "ENVIRONMENT_ROOT" "%ENVIRONMENT_ROOT%"
+powershell write-host -foregroundcolor Green "ENVIRONMENT_ROOT: %ENVIRONMENT_ROOT%"
+powershell write-host -foregroundcolor Green "-> Expanded: %SERVER_ROOT%\.config\environment"
 
-:: Substance Painter
-echo "Substance Painter - SUBSTANCE_PAINTER_PLUGINS_PATH:"
-setx SUBSTANCE_PAINTER_PLUGINS_PATH "%PIPELINE_ROOT%\substance_painter\python"
-:: echo %SUBSTANCE_PAINTER_PLUGINS_PATH%
-echo.
+setx "PIPELINE_ROOT" "%PIPELINE_ROOT%"
+powershell write-host -foregroundcolor Green "PIPELINE_ROOT: %PIPELINE_ROOT%"
+powershell write-host -foregroundcolor Green "-> Expanded: %SERVER_ROOT%\.config\pipeline"
 
+:: Set application-specific variables
+echo.
+echo ===========================================================================
+echo.
+powershell write-host -foregroundcolor Red "SideFX Houdini"
+setx "HSITE" "%HSITE%"
+powershell write-host -foregroundcolor Red "HSITE: %HSITE%"
+powershell write-host -foregroundcolor Red "-> Expanded: %SERVER_ROOT%\.config\pipeline\houdini"
+
+echo.
+echo ===========================================================================
+echo.
+powershell write-host -foregroundcolor White "Autodesk Maya"
+setx "MAYA_APP_DIR" "%MAYA_APP_DIR%"
+powershell write-host -foregroundcolor Cyan "MAYA_APP_DIR: %MAYA_APP_DIR%"
+powershell write-host -foregroundcolor Cyan "-> Expanded: %SERVER_ROOT%\.config\pipeline\maya"
+
+echo.
+echo ===========================================================================
+echo.
+powershell write-host -foregroundcolor White "Foundry Nuke"
+setx "NUKE_PATH" "%NUKE_PATH%"
+powershell write-host -foregroundcolor Yellow "NUKE_PATH: %NUKE_PATH%"
+powershell write-host -foregroundcolor Yellow "-> Expanded: %SERVER_ROOT%\.config\pipeline\nuke"
+
+echo.
+echo ===========================================================================
+echo.
+powershell write-host -foregroundcolor White "Adobe Substance Painter"
+setx "SUBSTANCE_PAINTER_PLUGINS_PATH" "%SUBSTANCE_PAINTER_PLUGINS_PATH%"
+powershell write-host -foregroundcolor DarkGreen "SUBSTANCE_PAINTER_PLUGINS_PATH: %SUBSTANCE_PAINTER_PLUGINS_PATH%"
+powershell write-host -foregroundcolor DarkGreen "-> Expanded: %SERVER_ROOT%\.config\pipeline\substance_painter\python"
+
+:: End
+echo.
+echo ===========================================================================
+echo.
 pause
-
